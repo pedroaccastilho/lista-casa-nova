0df2eb5866dc842e961a2d81b5bb8f4321eb3e98
pedroaccastilho <pedroaccastilho@gmail.com>
2026-04-04 21:48:51 -0300

    Adiciona fotos "Entrega das chaves" ao repositório

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -7,6 +7,8 @@ export interface Photo {
 // Coloque os arquivos em: public/photos/
 // Exemplo: public/photos/sala.jpg → src: '/lista-casa-nova/photos/sala.jpg'
 export const photos: Photo[] = [
+  { src: '/lista-casa-nova/photos/entrega-chaves.jpg', caption: 'Entrega das chaves' },
+  { src: '/lista-casa-nova/photos/entrega-chaves2.jpg', caption: 'Entrega das chaves' },
   { src: '/lista-casa-nova/photos/por-do-sol.jpg', caption: 'Nossa vista' },
   { src: '/lista-casa-nova/photos/piso-banheiros.jpeg', caption: 'Piso banheiros (obra)' },
   { src: '/lista-casa-nova/photos/cozinha-obra.jpeg', caption: 'Cozinha (obra)' },
